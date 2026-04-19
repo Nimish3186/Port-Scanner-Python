b89107da6d24ed34149e5062bba9aa7730457b28
Nimish3186 <nimishjindal3186@gmail.com>
2026-04-20 02:12:51 +0530

    banner grab

--- portscanner.py
+++ portscanner.py
@@ -3,14 +3,20 @@ from IPy import IP
 
 ports = [80, 8080, 443, 5500,22,23, 53]
 
+def grabBanner(s):
+    return s.recv(1024)
+
 def port_scan(target, port):
     try:
         sock = socket.socket()
         sock.settimeout(1)
         result = sock.connect_ex((target, port))
-
         if result == 0:
-            print(f"[+] Port {port} is open")
+            try:
+                banner = grabBanner(sock)
+                print(f"[+] Port {port} is open and is running {banner}")
+            except:
+                print(f"[+] Port {port} is open but noting to grab")
 
         sock.close()
 
